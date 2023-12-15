ddbfc6656293b0e0b12887de77959f935592eb8f
Drishya Gurung <120037176+Drishyagrg@users.noreply.github.com>
2023-12-15 22:00:42 +0545

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,8 @@
+#Day-1 project
+print("Welcome to ChildName Generator")
+person1=input("What is the first 3 letters of your name?\n")
+person2=input("What is the last 3 letters of your partner's name? \n")
+baby_name=person1+person2
+print('Your baby name could be ', baby_name)
+
+
